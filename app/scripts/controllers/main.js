eb82f49a93c1743fa5a4b0f211e6721d41592c3b
samdbeckham <sam@beckham.io>
2015-06-13 21:20:21 +0100

    Searching the guest name properly now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,8 +8,22 @@
  * Controller of the donnellyApp
  */
 angular.module('donnellyApp')
-    .controller('MainCtrl', function ($scope, Guest) {
-        $scope.guest;
+    .controller('MainCtrl', function ($scope, $filter, Guest) {
+        $scope.search = function(key) {
+            var params = {},
+                results = [];
+
+            params[key] = $scope.query;
+            results = $filter('startsWith')($scope.guests, params);
+
+            if (!results || results.length < 1) {
+                $scope.guest = '';
+            } else {
+                $scope.guest = results[0][key];
+            }
+        }
+
+// startsWith: { 'name': query }
 
         Guest.getAll().then(function(data) {
             $scope.guests = data;
